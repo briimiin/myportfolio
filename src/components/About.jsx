5d94bbdecd81adb1a1a5c9d9f878f95c6a186f9d
briimiin <kipkorirbrimin005@gmail.com>
2024-06-05 17:01:03 +0300

    fixed the hoc error and tilt import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt }from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -23,7 +23,7 @@ const ServiceCard = ({ index, title, icon }) => (
       >
         <img
           src={icon}
-          alt='web-development'
+          alt={title}
           className='w-16 h-16 object-contain'
         />
 
