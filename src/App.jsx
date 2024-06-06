8c0875964e4f7ab274f25e2abac3e6cc8f96e484
briimiin <kipkorirbrimin005@gmail.com>
2024-06-06 20:58:15 +0300

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import { BrowserRouter } from "react-router-dom";
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+// Lazy load components
+const Navbar = lazy(() => import('./components/Navbar'));
+const Hero = lazy(() => import('./components/Hero'));
+const About = lazy(() => import('./components/About'));
+const Experience = lazy(() => import('./components/Experience'));
+const Tech = lazy(() => import('./components/Tech'));
+const Works = lazy(() => import('./components/Works'));
+const Feedbacks = lazy(() => import('./components/Feedbacks'));
+const Contact = lazy(() => import('./components/Contact'));
+const StarsCanvas = lazy(() => import('./components/canvas/Stars'));
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Navbar />
+            <Hero />
+          </Suspense>
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
+        <Suspense fallback={<div>Loading...</div>}>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+        </Suspense>
         <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Contact />
+            <StarsCanvas />
+          </Suspense>
         </div>
       </div>
     </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/myportfolio/', // Make sure this matches your repository name
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // adjust the limit as needed
+  },
+});
